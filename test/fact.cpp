aa9a079e1a6b45a38eedf27dd651757ab41218e9
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-09-15 12:57:16 +0900

    boost

--- test/fact.cpp
+++ test/fact.cpp
@@ -1,10 +1,28 @@
 
 #include "../fact.hpp"
 #include <gtest/gtest.h>
-using namespace std;
+#include <boost/multiprecision/cpp_int.hpp>
+#include <stdexcept>
 using namespace algocpp::math;
 
 TEST(math_fact, integer)
 {
 	ASSERT_EQ(fact(5), 120);
+}
+
+TEST(math_fact, minus_integer)
+{
+	ASSERT_THROW(fact(-1), std::invalid_argument);
+}
+
+TEST(math_fact, cpp_int)
+{
+	boost::multiprecision::cpp_int n = 35;
+	ASSERT_EQ(fact(n), boost::multiprecision::cpp_int("10333147966386144929666651337523200000000"));
+}
+
+TEST(math_fact, minus_cpp_int)
+{
+	boost::multiprecision::cpp_int n = -1;
+	ASSERT_THROW(fact(n), std::invalid_argument);
 }
