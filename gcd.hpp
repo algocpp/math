5c5975cfe2ff0b354217e4ce29d4c0af16c84a08
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-09-16 09:57:07 +0900

    可変長引数と`std::vector`

--- gcd.hpp
+++ gcd.hpp
@@ -7,9 +7,12 @@
 #ifndef ALGOCPP_MATH_GCD
 #define ALGOCPP_MATH_GCD
 
+#include <stdexcept>
+#include <vector>
+#include <initializer_list>
+
 #if !defined(ALGOCPP_NO_LIB) && __has_include(<boost/multiprecision/cpp_int.hpp>)
 #include <boost/multiprecision/cpp_int.hpp>
-#include <stdexcept>
 #endif
 
 namespace algocpp
@@ -22,16 +25,52 @@ namespace algocpp
 		using max_integer = unsigned long long;
 #endif
 
-		inline max_integer gcd(max_integer a, max_integer b)
+		namespace base
 		{
-#ifdef BOOST_MP_CPP_INT_HPP
-			if (a < 0 || b < 0)
+			inline max_integer base_gcd(max_integer a, max_integer b)
 			{
-				throw std::invalid_argument("Any value must be a positive integer.");
-			}
+#ifdef BOOST_MP_CPP_INT_HPP
+				if (a < 0 || b < 0)
+				{
+					throw std::invalid_argument("Any value must be a positive integer.");
+				}
 #endif
 
-			return (b == 0 ? a : gcd(b, a % b));
+				return (b == 0 ? a : base_gcd(b, a % b));
+			}
+		}
+
+		inline max_integer gcd(max_integer a, max_integer b)
+		{
+			return base::base_gcd(a, b);
+		}
+
+		inline max_integer gcd(std::vector<max_integer> v)
+		{
+			if (v.size() < 2)
+			{
+				throw std::invalid_argument("The length of the argument must be at least 2.");
+			}
+
+			max_integer result = gcd(v[0], v[1]);
+			for (unsigned long long i = 2; i < v.size(); ++i)
+			{
+				result = gcd(result, v[i]);
+			}
+
+			return result;
+		}
+
+		template <typename... T>
+		inline max_integer gcd(T... v)
+		{
+			std::vector<max_integer> arg;
+			for (max_integer x : std::initializer_list<max_integer>{v...})
+			{
+				arg.emplace_back(x);
+			}
+
+			return gcd(arg);
 		}
 	}
 }
--- test/gcd.cpp
+++ test/gcd.cpp
@@ -3,25 +3,94 @@
 #include <gtest/gtest.h>
 #include <boost/multiprecision/cpp_int.hpp>
 #include <stdexcept>
+#include <vector>
+using namespace algocpp::math;
+using cint = boost::multiprecision::cpp_int;
 
 TEST(math_gcd, nomal)
 {
-	EXPECT_EQ(algocpp::math::gcd(12, 9), 3);
+	EXPECT_EQ(gcd(12, 9), 3);
 }
 
 TEST(math_gcd, equal)
 {
-	EXPECT_EQ(algocpp::math::gcd(123, 123), 123);
+	EXPECT_EQ(gcd(123, 123), 123);
 }
 
 TEST(math_gcd, big)
 {
-	boost::multiprecision::cpp_int x("438294790427304371897418"), y("7481978943743232");
-	EXPECT_EQ(algocpp::math::gcd(x, y), boost::multiprecision::cpp_int("6"));
+	cint x("438294790427304371897418"), y("7481978943743232");
+	EXPECT_EQ(gcd(x, y), cint("6"));
 }
 
 TEST(math_gcd, minus)
 {
-	boost::multiprecision::cpp_int x("-1"), y("490724074397439279374273207903749237390274907902793429");
-	EXPECT_THROW(algocpp::math::gcd(x, y), std::invalid_argument);
+	cint x("-1"), y("490724074397439279374273207903749237390274907902793429");
+	EXPECT_THROW(gcd(x, y), std::invalid_argument);
+}
+
+TEST(math_gcd, vector_max)
+{
+	std::vector<cint> x = {630, 300, 520, 3150};
+	EXPECT_EQ(gcd(x), cint("10"));
+}
+
+TEST(math_gcd, vector_long)
+{
+	std::vector<cint> x = {432, 4320, 320, 630, 150, 310, 594, 382, 472, 582, 588, 596, 482, 594, 194, 682, 192};
+	EXPECT_EQ(gcd(x), cint("2"));
+}
+
+TEST(math_gcd, vector_minus)
+{
+	std::vector<cint> x = {3, 2, 5, 6, 4, 2, 6, 8, 6, 54, -1, 483};
+	EXPECT_THROW(gcd(x), std::invalid_argument);
+}
+
+TEST(math_gcd, vector_one)
+{
+	std::vector<cint> x = {3};
+	EXPECT_THROW(gcd(x), std::invalid_argument);
+}
+
+TEST(math_gcd, vector_zero)
+{
+	std::vector<cint> x = {};
+	EXPECT_THROW(gcd(x), std::invalid_argument);
+}
+
+TEST(math_gcd, vector_int)
+{
+	std::vector<int> x{32, 16, 12};
+	EXPECT_EQ(gcd(x), 4);
+}
+
+TEST(math_gcd, arg)
+{
+	EXPECT_EQ(gcd(32, 16, 12), 4);
+}
+
+TEST(math_gcd, arg_long)
+{
+	EXPECT_EQ(gcd(32, 6, 12, 8, 4, 2, 12, 4, 56), 2);
+}
+
+TEST(math_gcd, arg_max)
+{
+	EXPECT_EQ(gcd(cint("31521452"), cint("43255762"), cint("4379834")), cint("2"));
+}
+
+TEST(math_gcd, arg_one)
+{
+	EXPECT_THROW(gcd(31415926535), std::invalid_argument);
+}
+
+TEST(math_gcd, arg_zero)
+{
+	EXPECT_THROW(gcd(), std::invalid_argument);
+}
+
+TEST(math_gcd, arg_minus)
+{
+	EXPECT_THROW(gcd(42283, 43920432, 84902, 29, 42, -1, 438), std::invalid_argument);
 }
