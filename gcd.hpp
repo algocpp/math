79e1e22ca82a439543257207d80d2a2dcddb7494
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-09-16 10:43:16 +0900

    `boost::array`

--- gcd.hpp
+++ gcd.hpp
@@ -15,6 +15,10 @@
 #include <boost/multiprecision/cpp_int.hpp>
 #endif
 
+#if !defined(ALGOCPP_NO_LIB) && __has_include(<boost/array.hpp>)
+#include <boost/array.hpp>
+#endif
+
 namespace algocpp
 {
 	namespace math
@@ -75,8 +79,8 @@ namespace algocpp
 			return result;
 		}
 
-		template <typename T,std::size_t N>
-		inline T gcd(std::array<T,N> v)
+		template <typename T, std::size_t N>
+		inline T gcd(std::array<T, N> v)
 		{
 			if (v.size() < 2)
 			{
@@ -91,6 +95,25 @@ namespace algocpp
 
 			return result;
 		}
+
+#ifdef BOOST_ARRAY_HPP
+		template <typename T, std::size_t N>
+		inline T gcd(boost::array<T, N> v)
+		{
+			if (v.size() < 2)
+			{
+				throw std::invalid_argument("The length of the argument must be at least 2.");
+			}
+
+			T result = gcd(v[0], v[1]);
+			for (unsigned long long i = 2; i < v.size(); ++i)
+			{
+				result = gcd(result, v[i]);
+			}
+
+			return result;
+		}
+#endif
 	}
 }
 
--- test/gcd.cpp
+++ test/gcd.cpp
@@ -2,11 +2,14 @@
 #include "../gcd.hpp"
 #include <gtest/gtest.h>
 #include <boost/multiprecision/cpp_int.hpp>
+#include <boost/array.hpp>
 #include <stdexcept>
 #include <vector>
 #include <array>
 using namespace algocpp::math;
 using cint = boost::multiprecision::cpp_int;
+
+// std::vector
 
 TEST(math_gcd, nomal)
 {
@@ -66,6 +69,8 @@ TEST(math_gcd, vector_int)
 	EXPECT_EQ(gcd(x), 4);
 }
 
+// std::array
+
 TEST(math_gcd, array_max)
 {
 	std::array<cint, 4> x = {630, 300, 520, 3150};
@@ -98,6 +103,44 @@ TEST(math_gcd, array_zero)
 
 TEST(math_gcd, array_int)
 {
-	std::array<int, 2> x{32, 16, 12};
+	std::array<int, 3> x{32, 16, 12};
+	EXPECT_EQ(gcd(x), 4);
+}
+
+// boost::array
+
+TEST(math_gcd, boost_array_max)
+{
+	boost::array<cint, 4> x = {630, 300, 520, 3150};
+	EXPECT_EQ(gcd(x), cint("10"));
+}
+
+TEST(math_gcd, boost_array_long)
+{
+	boost::array<cint, 17> x = {432, 4320, 320, 630, 150, 310, 594, 382, 472, 582, 588, 596, 482, 594, 194, 682, 192};
+	EXPECT_EQ(gcd(x), cint("2"));
+}
+
+TEST(math_gcd, boost_array_minus)
+{
+	boost::array<cint, 12> x = {3, 2, 5, 6, 4, 2, 6, 8, 6, 54, -1, 483};
+	EXPECT_THROW(gcd(x), std::invalid_argument);
+}
+
+TEST(math_gcd, boost_array_one)
+{
+	boost::array<cint, 1> x = {3};
+	EXPECT_THROW(gcd(x), std::invalid_argument);
+}
+
+TEST(math_gcd, boost_array_zero)
+{
+	boost::array<cint, 0> x = {};
+	EXPECT_THROW(gcd(x), std::invalid_argument);
+}
+
+TEST(math_gcd, boost_array_int)
+{
+	boost::array<int, 3> x{32, 16, 12};
 	EXPECT_EQ(gcd(x), 4);
 }
